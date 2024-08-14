b3decf87092be59d32e30591fcca953d81412507
Gaurav Kharal <gauravkharal9@gmail.com>
2024-08-14 14:30:54 +0545

    Retrofit/NetworkClient Done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,9 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.9.1")
     implementation("com.squareup.okhttp3:logging-interceptor:4.9.1")
 
+    debugImplementation("com.github.chuckerteam.chucker:library:4.0.0")
+    releaseImplementation("com.github.chuckerteam.chucker:library-no-op:4.0.0")
+
 
 
     implementation(libs.appcompat)
--- app/src/main/java/com/example/linkstation/network/NetworkClient.java
+++ app/src/main/java/com/example/linkstation/network/NetworkClient.java
@@ -1,5 +1,10 @@
 package com.example.linkstation.network;
 
+import android.content.Context;
+
+import com.chuckerteam.chucker.api.ChuckerInterceptor;
+
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -7,10 +12,14 @@ public class NetworkClient {
     private static Retrofit retrofit;
     private static final String BASE_URL = "https://mint-briefly-cheetah.ngrok-free.app";
 
-    public static Retrofit getRetrofitClient() {
+    public static Retrofit getService(Context context) {
         if (retrofit == null) {
+            OkHttpClient client = new OkHttpClient.Builder()
+                    .addInterceptor(new ChuckerInterceptor(context))
+                    .build();
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
+                    .client(client)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
