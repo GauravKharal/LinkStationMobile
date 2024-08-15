73fbbc55beb17ca094dd3e92d2bbb776e8b194f8
Gaurav Kharal <gauravkharal9@gmail.com>
2024-08-15 13:19:09 +0545

    Fixed Issues + Done

--- app/src/main/java/com/example/linkstation/utility/TokenManager.java
+++ app/src/main/java/com/example/linkstation/utility/TokenManager.java
@@ -2,27 +2,36 @@ package com.example.linkstation.utility;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.util.Log;
 
 public class TokenManager {
     private static final String PREFS_NAME = "MyAppPrefs";
-    private static final String TOKEN_KEY = "auth_token";
+    private static final String ACCESS_TOKEN_KEY = "accessToken";
+    private static final String REFRESH_TOKEN_KEY = "refreshToken";
 
-    public static void saveToken(Context context, String token){
+    public static void saveToken(Context context, String accessToken, String refreshToken){
+        Log.d("TokenManager", "Saving tokens: AccessToken=" + accessToken + " RefreshToken=" + refreshToken);
         SharedPreferences sharedPreferences = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putString(TOKEN_KEY,token);
+        editor.putString(ACCESS_TOKEN_KEY,accessToken);
+        editor.putString(REFRESH_TOKEN_KEY,refreshToken);
         editor.apply();
     }
 
-    public static String getToken(Context context){
+    public static String getAccessToken(Context context){
         SharedPreferences sharedPreferences = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
-        return sharedPreferences.getString(TOKEN_KEY, null);
+        return sharedPreferences.getString(ACCESS_TOKEN_KEY,null);
+    }
+    public static String getRefreshToken(Context context){
+        SharedPreferences sharedPreferences = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+        return sharedPreferences.getString(REFRESH_TOKEN_KEY,  null);
     }
 
     public static void clearToken(Context context){
         SharedPreferences sharedPreferences = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.remove(TOKEN_KEY);
+        editor.remove(ACCESS_TOKEN_KEY);
+        editor.remove(REFRESH_TOKEN_KEY);
         editor.apply();
     }
 
