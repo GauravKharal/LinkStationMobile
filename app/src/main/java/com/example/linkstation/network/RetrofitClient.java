d8e88bf3880ac496311fbcb7089976751d9b7fbc
Gaurav Kharal <gauravkharal9@gmail.com>
2024-08-15 13:17:54 +0545

    Renamed + Minor Updates

--- app/src/main/java/com/example/linkstation/network/RetrofitClient.java
+++ app/src/main/java/com/example/linkstation/network/RetrofitClient.java
@@ -8,11 +8,11 @@ import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class NetworkClient {
+public class RetrofitClient {
     private static Retrofit retrofit;
     private static final String BASE_URL = "https://mint-briefly-cheetah.ngrok-free.app";
 
-    public static Retrofit getRetrofitClient(Context context) {
+    public static Retrofit getClient(Context context) {
         if (retrofit == null) {
             OkHttpClient client = new OkHttpClient.Builder()
                     .addInterceptor(new ChuckerInterceptor(context))
