dbb51aeb51ed7d9ee6ba9a43c5005e33acc87872
Gaurav Kharal <gauravkharal9@gmail.com>
2024-08-17 10:13:57 +0545

    full screen

--- app/src/main/java/com/example/linkstation/ui/LoginActivity.java
+++ app/src/main/java/com/example/linkstation/ui/LoginActivity.java
@@ -40,7 +40,9 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
+
 
 
         etUsername = findViewById(R.id.etUsername);
